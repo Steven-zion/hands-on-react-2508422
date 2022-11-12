766628a3ff995bd28659f97a0e3d02e37fbfa9c9
Steven Zion <68914811+Steven-zion@users.noreply.github.com>
2022-11-12 22:11:37 +0000

    hoooks & conditional rendering

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -3,8 +3,13 @@ import { useState } from 'react';
 export default () => {
   const [count, setCount] = useState(0)
   return (
-    <button className="outline" onClick={() => setCount(count + 1)}>
-      {count === 0 ? "Click to support": `Supported ${count} times`}
-    </button>
-  )
+      <>
+          <button className="outline" onClick={() => setCount(count + 1)}>
+              {count === 0 ? 'Click to support' : `Supported ${count} times`}
+          </button>
+          <button className="outline" onClick={() => setCount(count + 1)}>
+              {count % 3 ? 'Click to Donate' : `Donated ${count} times`}
+          </button>
+      </>
+  );
 }
