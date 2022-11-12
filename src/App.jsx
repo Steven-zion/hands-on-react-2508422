4df3ff6ed77ca9d28da296498f2359b132d16800
Steven Zion <68914811+Steven-zion@users.noreply.github.com>
2022-11-12 21:36:52 +0000

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 function App() {
+  const name = 'intergalactic alliance';
+  
   return (
     <hgroup>
       <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <p>Members of an <i style={ {color: 'steelBlue'}}>{name}</i> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <button className="outline" onClick={()=> alert('hi there!')}></button>
     </hgroup>
   )
 }
