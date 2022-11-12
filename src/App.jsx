c5a2ad1ea71ef3a37937f0cca0ed19d934848bcd
Steven Zion <68914811+Steven-zion@users.noreply.github.com>
2022-11-12 21:38:59 +0000

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ function App() {
   
   return (
     <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <i style={ {color: 'steelBlue'}}>{name}</i> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <h1>Meet the <i style={ {color: 'steelBlue'}}>StarGazers</i></h1>
+      <p>Members of an <b>{name}</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
       <button className="outline" onClick={()=> alert('hi there!')}></button>
     </hgroup>
   )
